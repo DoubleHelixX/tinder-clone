22bb24e9cecbf9781d9f698e0b8390da2529554e
DoubleHelix <tjrnyc5@gmail.com>
2021-05-05 18:58:02 -0400

    importing material UI Icons for Header component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React from "react"; 
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
-      <h1> lets build </h1>
+      <Header/>
+      
     </div>
   );
 }
