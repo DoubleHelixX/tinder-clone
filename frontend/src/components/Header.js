6452d118097d5831b46fb67fd216aabdebd99cca
DoubleHelix <tjrnyc5@gmail.com>
2021-05-06 14:46:33 -0400

    added button icons for design purposes

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -4,6 +4,7 @@ import styled from "styled-components";
 
 import PersonIcon from "@material-ui/icons/Person";
 import ForumIcon from "@material-ui/icons/Forum";
+import IconButton from "@material-ui/core/IconButton";
 
 import tinderImg from "../images/tinder-logo.svg";
 
@@ -13,20 +14,36 @@ S.NavContainer = styled.div`
   display: flex;
   justify-content: space-between;
   border-bottom: 1px solid #f9f9f9;
+  align-items: center;
 `;
+
+S.NavIcon = styled.div`
+  padding: 20px;
+`;
+
 S.TinderLogo = styled.img`
   content: url(${tinderImg});
   /* background-size: cover; */
-  height: 40px;
+  height: 50px;
   object-fit: contain;
 `;
 
 const Header = () => {
   return (
     <S.NavContainer>
-      <PersonIcon />
+      <S.NavIcon>
+        <IconButton>
+          <PersonIcon fontSize="large" />
+        </IconButton>
+      </S.NavIcon>
+      {/* <IconButton> */}
       <S.TinderLogo />
-      <ForumIcon />
+      {/* </IconButton> */}
+      <S.NavIcon>
+        <IconButton>
+          <ForumIcon fontSize="large" />
+        </IconButton>
+      </S.NavIcon>
     </S.NavContainer>
   );
 };
